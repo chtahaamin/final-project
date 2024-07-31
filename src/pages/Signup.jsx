ac4e43fcde1e769b8f665215f0c42abf5113ef58
chtahaamin <247tahaamin@gmail.com>
2024-07-31 15:55:52 +0500

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -29,7 +29,6 @@ const SignUp = () => {
       localStorage.setItem('email', email.current.value);
       localStorage.setItem('password', password.current.value);
       localStorage.setItem('signUp', email.current.value);
-      alert('Your account is created successfully');
       navigate('/login', { replace: true });
     } else {
       alert('Please enter your credentials');

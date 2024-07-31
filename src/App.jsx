65ffd1292fe6913db95c0ba3d67f1ac8050b9071
MTaha <247tahaamin@gmail.com>
2024-07-31 20:05:27 +0500

    reverted back to the previous code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import SignUp from './pages/Signup'
+import SignUp from './pages/Signup';
 import Home from './pages/Home';
+import ErrorPage from './Components/ErrorComponent';
 import { LoginProvider } from './Components/LoginContext';
-import { EmailProvider} from './Components/EmailContetx'
+import { EmailProvider } from './Components/EmailContetx';
 
 const App = () => {
   return (
     <LoginProvider>
       <EmailProvider>
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-    </EmailProvider>
+        <Router>
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </Router>
+      </EmailProvider>
     </LoginProvider>
   );
 };
